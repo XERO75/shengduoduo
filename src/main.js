0fccec75abffbf40e9596bf2c6c0fb15ba723383
Zero <zero@hboxs.com>
2018-12-18 10:57:50 +0800

    修改打包路径

--- src/main.js
+++ src/main.js
@@ -5,15 +5,6 @@ import App from './App.vue';
 import { router } from './newRouter';
 import store from "./store.js";
 
-import VueAMap from 'vue-amap';
-Vue.use(VueAMap);
-VueAMap.initAMapApiLoader({
-  key: 'fa88b622064b112e8caefeb05f40b790',
-  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
-  // 默认高德 sdk 版本为 1.4.4
-  v: '1.4.10'
-});
-
 new Vue({
   router,
   store,
